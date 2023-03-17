398f3090f36868f42a11c3bca84a715a31e57f6a
Shivam Shukla <shivamshukla3108@gmail.com>
2023-03-17 06:54:15 +0530

    Make a guest app, and do many managements on there...

--- @theshivashu07/guest/urls.py
+++ @theshivashu07/guest/urls.py
@@ -4,6 +4,22 @@ from . import views
 urlpatterns=[
 
 		path('',views.index,name='index'),
+		# path('index/',views.index,name='index'),
+		path('services/',views.services,name='services'),
+		path('projects2/',views.projects2,name='projects2'),
+		path('projects3/',views.projects3,name='projects3'),
+		path('projectdetails/',views.projectdetails,name='projectdetails'),
+		path('blog/',views.blog,name='blog'),
+		path('blogsingle/',views.blogsingle,name='blogsingle'),
+		path('ourteam/',views.ourteam,name='ourteam'),
+		path('archives/',views.archives,name='archives'),
+		path('grids/',views.grids,name='grids'),
+		path('error404/',views.error404,name='error404'),
+		path('contact/',views.contact,name='contact'),
+
+		
+		path('index/',views.index,name='index'),
+		# path('/index',views.index,name='index'),
 		#path('/',views.,name=''),
 		#path('/',views.,name=''),
 		#path('/',views.,name=''),
--- @theshivashu07/guest/views.py
+++ @theshivashu07/guest/views.py
@@ -11,6 +11,57 @@ from django.http import HttpResponse
 def index(request):
 	return render(request,"guest/index.html");
 
+def services(request):
+	return render(request,"guest/services.html");
+
+def projects2(request):
+	return render(request,"guest/projects-2.html");
+
+def projects3(request):
+	return render(request,"guest/projects-3.html");
+
+def projectdetails(request):
+	return render(request,"guest/project-details.html");
+
+def blog(request):
+	return render(request,"guest/blog.html");
+
+def blogsingle(request):
+	return render(request,"guest/blog-single.html");
+
+def ourteam(request):
+	return render(request,"guest/our-team.html");
+
+def archives(request):
+	return render(request,"guest/archives.html");
+
+def grids(request):
+	return render(request,"guest/grids.html");
+
+def error404(request):
+	return render(request,"guest/404.html");
+
+def contact(request):
+	return render(request,"guest/contact.html");
+
+
+
+#########################################################################################
+
+
+
+def index(request):
+	return render(request,"guest/_index.html");
+
+# def index(request):
+# 	return render(request,"guest/index.html");
+
+# def index(request):
+# 	return render(request,"guest/index.html");
+
+# def index(request):
+# 	return render(request,"guest/index.html");
+
 
 
 
