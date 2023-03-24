9c8aa4172f43155303e7b2dd5fa02908d2e6ca44
Shivam Shukla <shivamshukla3108@gmail.com>
2023-03-24 22:24:28 +0530

    'edittables' link almost donegit log

--- @theshivashu07/codecollections/urls.py
+++ @theshivashu07/codecollections/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns=[
 
 		path('',views.index,name='index'),
+		path('edittables/',views.edittables,name='edittables'),
 		path('codesubmissions/',views.codesubmissions,name='codesubmissions'),
-		
+		path('problemsubmissions/',views.problemsubmissions,name='problemsubmissions'),
+		# path('index/',views.index,name='index'),
+		# path('index/',views.index,name='index'),
+		# path('index/',views.index,name='index'),
 		# path('index/',views.index,name='index'),
 		# path('index/',views.index,name='index'),
 		# path('index/',views.index,name='index'),
--- @theshivashu07/codecollections/views.py
+++ @theshivashu07/codecollections/views.py
@@ -10,14 +10,36 @@ import codecollections._importantdatasets as _importantdatasets
 	# return HttpResponse("Hey <b>Shivam Shukla</b>, <b style='font-size:10px'>N your url '@theshivashu07'</b> !!!");
 
 
-def index(request):
-	return render(request,"otherapps/codecollections/index.html");
-
-def codesubmissions(request):
-	SenderDatasets={
+SenderDatasets={
 		'DataStructures':_importantdatasets.DataStructures,
 		'Plateforms':_importantdatasets.Plateforms,
 		'ProgrammingLanguages':_importantdatasets.ProgrammingLanguages,
 	}
+
+def index(request):
+	return render(request,"otherapps/codecollections/index.html");
+
+def codesubmissions(request):
 	return render(request,"otherapps/codecollections/codesubmissions.html", SenderDatasets);
 
+def problemsubmissions(request):
+	return render(request,"otherapps/codecollections/problemsubmissions.html", SenderDatasets);
+
+def edittables(request):
+	print(request)
+	if request.method=="POST":
+		comingFrom=request.POST["comingFrom"]
+		comingData=request.POST["comingData"]
+		# print("comingFrom",comingFrom,"     ","comingData",comingData)
+		hold=SenderDatasets[comingFrom+'s']
+		hold[comingData]=None
+		SenderDatasets[comingFrom+'s']=hold
+		# return redirect("/codecollections/edittables/")
+	return render(request,"otherapps/codecollections/edittables.html", SenderDatasets);
+
+
+'''
+	if request.method=="POST":
+		values=ProjectInfo(
+						Client=request.POST["clientID"],
+'''
