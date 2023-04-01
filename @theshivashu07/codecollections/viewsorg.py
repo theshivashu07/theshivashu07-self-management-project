7ebe0d920f78238687d5407094d391561459ceb1
Shivam Shukla <shivamshukla3108@gmail.com>
2023-04-01 23:33:06 +0530

    Updations Continued...

--- @theshivashu07/codecollections/viewsorg.py
+++ @theshivashu07/codecollections/viewsorg.py
@@ -34,6 +34,103 @@ def justtry(request):
 
 def addproblems(request):
 	if request.method=="POST":
+		if( request.POST["comingProblemTitle"] and request.POST["comingDetails"] ):
+			comingProblemTitle=request.POST["comingProblemTitle"]
+			comingPlateforms=request.POST.getlist("comingPlateforms")
+			comingDataStructures=request.POST.getlist("comingDataStructures")
+			if(comingProblemTitle or comingPlateforms or comingDataStructures):
+				locks = Problems.objects.get(pk=problemID.id)  #Problems()
+				# locks = Problems()
+				if(comingProblemTitle):
+					locks.title=comingProblemTitle
+				if(comingPlateforms):
+					locks.plateforms=len(comingPlateforms)
+					holds = [  str(object.plateform_id) for object in problems_plateforms.objects.filter(problem_id=problemID.id) ]
+					for id in comingPlateforms:
+						if(id in holds):
+							holds.remove(id)
+						else:
+							lock=problems_plateforms()
+							lock.problem_id=problemID
+							lock.plateform_id=id
+							lock.save()
+					else:
+						for id in holds:
+							object = problems_plateforms.objects.get(plateform_id=id, problem_id=problemID.id)
+							object.delete()
+				if(comingDataStructures):
+					locks.datastructures=len(comingDataStructures)
+					holds = [ str(object.datastructure_id) for object in problems_datastructures.objects.filter(problem_id=problemID.id) ]
+					for id in comingDataStructures:
+						if(id in holds):
+							holds.remove(id)
+						else:
+							lock=problems_datastructures()
+							lock.problem_id=problemID
+							lock.datastructure_id=id
+							lock.save()
+					else:
+						for id in holds:
+							object = problems_datastructures.objects.get(datastructure_id=id, problem_id=problemID.id)
+							object.delete()
+
+				locks.save()
+
+			comingDetails=request.POST["comingDetails"]
+			comingTimeComplexity=request.POST["comingTimeComplexity"]
+			comingAuxiliarySpace=request.POST["comingAuxiliarySpace"]
+			if(comingDetails or comingTimeComplexity or comingAuxiliarySpace):
+				locks = Problems.objects.get(pk=problemID.id)  #Problems()
+				# locks = Problems()
+				if(comingDetails):
+					locks.detailsset+=1
+					lock=problems_detailssets()
+					lock.problem_id=problemID
+					lock.detailsset=comingDetails
+					lock.save()
+				if(comingTimeComplexity):
+					locks.timecomplexity=comingTimeComplexity
+				if(comingAuxiliarySpace):
+					locks.auxiliaryspace=comingAuxiliarySpace
+				locks.save()
+		else:
+			print("This is not correct Input's... Reput again!!!")
+		# return redirect("/codecollections/problemsubmissions/")
+		return redirect("/codecollections/problems/new/")
+
+	locks=Problems.objects.get(id=problemID.id)
+	holds=problems_plateforms.objects.filter(problem_id=problemID.id)
+	locks.plateforms=[ Plateforms.objects.get(pk=object.plateform_id) for object in holds ]
+	holds=problems_datastructures.objects.filter(problem_id=problemID.id)
+	locks.datastructures=[ DataStructures.objects.get(pk=object.datastructure_id) for object in holds ]
+	locks.detailsset=problems_detailssets.objects.filter(problem_id=problemID.id)
+	SenderDatasets={
+		'DataSet':locks,
+		'Plateforms':Plateforms.objects.all(),
+		'DataStructures':DataStructures.objects.all(),
+		'ProgrammingLanguages':ProgrammingLanguages.objects.all(),
+	}
+	# return render(request,"otherapps/codecollections/new_404.html");
+	return render(request,"otherapps/codecollections/new_addproblems.html",SenderDatasets);
+'''
+def addproblems(request):
+	if request.method=="POST":
+		pass
+	SenderDatasets={
+		'DataSet':None,
+		'Plateforms':Plateforms.objects.all(),
+		'DataStructures':DataStructures.objects.all(),
+		'ProgrammingLanguages':ProgrammingLanguages.objects.all(),
+	}
+	# return render(request,"otherapps/codecollections/new_404.html");
+	return render(request,"otherapps/codecollections/new_addproblems.html",SenderDatasets);
+'''
+
+
+
+# def addsolutions(request,problemslug):
+def addsolutions(request,problemslug='problem-number-0001'):
+	if request.method=="POST":
 		comingProblemID= problemID.id; #request.POST["comingProblemID"]
 		comingDataStructures=request.POST.getlist("comingDataStructures")
 		comingProgrammingLanguage=request.POST["comingProgrammingLanguage"]
@@ -74,7 +171,7 @@ def addproblems(request):
 						object = solutions_datastructures.objects.get(datastructure_id=id, solution_id=locks.id)
 						object.delete()
 				locks.save()
-		return redirect("/codecollections/problems/new/")
+		return redirect("/codecollections/problemsandsolutions/problem-number-001/new/")
 
 	locks=Problems.objects.get(id=problemID.id)
 	holds=problems_plateforms.objects.filter(problem_id=problemID.id)
@@ -83,30 +180,14 @@ def addproblems(request):
 	locks.datastructures=[ DataStructures.objects.get(pk=object.datastructure_id) for object in holds ]
 	locks.detailsset=problems_detailssets.objects.filter(problem_id=problemID.id)
 	SenderDatasets={
-		'DataSet':None,
-		# 'Plateforms':Plateforms.objects.all(),
-		# 'DataStructures':DataStructures.objects.all(),
-		# 'ProgrammingLanguages':ProgrammingLanguages.objects.all(),
-	}
-	# return render(request,"otherapps/codecollections/new_404.html");
-	return render(request,"otherapps/codecollections/new_addproblems.html",SenderDatasets);
-'''
-def addproblems(request):
-	if request.method=="POST":
-		pass
-	SenderDatasets={
-		'DataSet':None,
+		'DataSet':locks,
 		'Plateforms':Plateforms.objects.all(),
 		'DataStructures':DataStructures.objects.all(),
 		'ProgrammingLanguages':ProgrammingLanguages.objects.all(),
 	}
 	# return render(request,"otherapps/codecollections/new_404.html");
-	return render(request,"otherapps/codecollections/new_addproblems.html",SenderDatasets);
+	return render(request,"otherapps/codecollections/new_addsolutions.html",SenderDatasets);
 '''
-
-
-
-
 # def addsolutions(request,problemslug):
 def addsolutions(request,problemslug='problem-number-0001'):
 	if request.method=="POST":
@@ -116,7 +197,7 @@ def addsolutions(request,problemslug='problem-number-0001'):
 	}
 	# return render(request,"otherapps/codecollections/new_404.html");
 	return render(request,"otherapps/codecollections/new_addsolutions.html",SenderDatasets);
-
+'''
 
 
 
